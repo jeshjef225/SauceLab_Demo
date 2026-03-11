4f8b1389cb91b86b5706aab5db17cac521946d67
J. Jeshwin <52910753+jeshjef225@users.noreply.github.com>
2026-03-11 16:31:03 +0530

    Update login.step.ts

--- step-definitions/login.step.ts
+++ step-definitions/login.step.ts
@@ -1,6 +1,6 @@
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
-import { CustomWorld } from '../features/support/world';
+import { CustomWorld } from '../support/world';
 
 const BASE_URL = 'https://www.saucedemo.com';
 
