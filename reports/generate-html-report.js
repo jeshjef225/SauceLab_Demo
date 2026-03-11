7567fbaf76d10d6c93a3dffde58a8e9ee12b9ad4
J. Jeshwin <52910753+jeshjef225@users.noreply.github.com>
2026-03-11 16:39:32 +0530

    Create generate-html-report.js

---
+++ reports/generate-html-report.js
@@ -0,0 +1,15 @@
+const reporter = require('cucumber-html-reporter');
+
+const options = {
+  theme: 'bootstrap',
+  jsonFile: 'reports/cucumber_report.json',
+  output: 'reports/cucumber_report.html',
+  reportSuiteAsScenarios: true,
+  launchReport: false,
+  metadata: {
+    "Browser": "Chromium",
+    "Platform": process.platform
+  }
+};
+
+reporter.generate(options);
